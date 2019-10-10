c4782261eeff74e1b0688387f3f3ada61a3049b8
DESKTOP-IF8LPES\Admin <t.mathieu@ludus-academie.com>
2019-10-10 14:36:06 +0200

    (test premier git UE avec le gitignore)

---
+++ Projet_CPP/Source/Projet_CPP/Projet_CPPGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Projet_CPPGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJET_CPP_API AProjet_CPPGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
